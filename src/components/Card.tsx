8c44934d410ae30017e7cb971523b6b9cbbf0729
devgl96 <georgelucas.dev@gmail.com>
2025-05-03 23:21:27 -0300

    fix: fix badgeColor issue

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from "tailwind-merge";
+
 interface CardProps {
   title: string;
   badge: string;
@@ -8,12 +10,36 @@ interface CardProps {
 
 export default function Card(props: CardProps) {
   const { title, badge, description, badgeColor, position } = props;
+
+  const textColor = `text-${badgeColor}-800`;
+  const backgroundColor = `bg-${badgeColor}-100`;
+
+  const getBadgeColor = () => {
+    switch (badgeColor) {
+      case "red":
+        return "text-red-800 bg-red-100";
+      case "blue":
+        return "text-blue-800 bg-blue-100";
+      case "green":
+        return "text-green-800 bg-green-100";
+      case "yellow":
+        return "text-yellow-800 bg-yellow-100";
+      case "purple":
+        return "text-purple-800 bg-purple-100";
+      default:
+        return "text-gray-800 bg-gray-100";
+    }
+  };
+
   return (
     <div
       className={`w-52 h-56 bg-white rounded-lg absolute ${position} p-4 flex flex-col items-start justify-center gap-2 shadow-xl`}
     >
       <span
-        className={`text-${badgeColor}-800 font-normal text-sm bg-${badgeColor}-100 rounded-sm px-2 py-2 flex items-center justify-center h-6`}
+        className={twMerge(
+          "font-normal text-sm rounded-sm px-2 py-2 flex items-center justify-center h-6",
+          getBadgeColor()
+        )}
       >
         {badge}
       </span>
